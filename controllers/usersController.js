de1d28f5fa5df682f5c23046784328e87dae6cd7
Dognizumoh <idafolo16@gmail.com>
2022-04-22 14:29:58 +0000

    error codes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,27 +1,66 @@
-const users =require("../data/users")
+const User =require("../models/userSchema")
+const bcrypt = require("bcrypt")
+const validate = require("../config/validator")
+const userhandler = require("../handlers/user.handler")
 
 
 //get all users
 const getUsers=(req, res)=>{
-    res.status(200).json({success:true, data:users});
+    try{
+        let allusers = await. User.find({});
+        res.status(200).json({success:true, body:allusers});
+    }catch(error){
+        res.status(500).json({success:false, body:error});
+    }
+    
 }
 
 //create a user
-const createUser =(req, res)=>{
-    const newUser = req.body;
-    users.push(newUser)
-    res.status(200).json(users)
-}
-//get user by the id
-const getUser = (req, res)=>{
-    const userId = req.params.id;
-    const user = users.find((u)=>u.id==userId);
+const createUser =async(req, res)=>{
+    try{
+    //hashing password with bcrypt
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password,salt);
 
-    if(!user){
-        return res.status(400).json({success:false, message:"user not found"});
-    }
-    res.status(200).json({success:true, data:user});
+    //validating a user
+    const valid = await validate({username,email,password})
+
+    if(valid){
+
+   const newUser= new User({
+       username:req.body.username,
+       email:req.body.email,
+       password:hashedPassword
+   });
+   await newUser.save();
+   res.status(201).json({
+       id:newUser._id,
+       username:newUser.username,
+       email:newUser.email
+
+   })
+}else{
+    res.status(400).json({
+        message:"Invalid data"
+    });
+}}catch(error){
+    let message = errorData(error)
 }
+}
+
+
+//get user by the id
+const getUser = async(req, res)=>{
+    try{
+    const userId = req.params.id;
+    const user = await User.findById(userId)
+
+    res.status(200).json({success:true, data:user});
+} catch (error){
+        return res.status(400).json({success:false, body:"error"});
+}
+    }
+    
 
 module.exports ={getUsers, createUser, getUser}
 
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const usersRoute = require("./routes/usersRoutes")
 const path = require("path")
 const logger = require ("./middlewares/user.logger")
 const booksRoute = require("./routes/booksRoute")
+require("dotenv").config()
+const connectDB =require("./config/connectDB")
+
+connectDB()
 
 const app = express();
 
